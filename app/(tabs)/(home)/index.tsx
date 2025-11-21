d3a6155a65d3672672df7c2d97ac68e2d2670467
Vinícius Abdala <abdalazard@gmail.com>
2025-11-21 02:38:58 -0300

    feat: add home screen component for task creation and listing.

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'; 
-import { View, Text } from 'react-native'; 
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
 import styles from '../../style';
 import CriaTarefa from '@/components/CriaTarefa';
 import ListaDeTarefas from '@/components/ListaDeTarefas';
 import { Task } from '@/hooks/store/types/task.type';
 
 export default function Index() {
-  const [modalVisible, setModalVisible] = useState(false); 
+  const [modalVisible, setModalVisible] = useState(false);
   const [tarefaEmEdicao, setTarefaEmEdicao] = useState<Task | null>(null);
 
   const handleOpenModal = (tarefa?: Task) => {
     setTarefaEmEdicao(tarefa || null);
     setModalVisible(true);
   };
-  
+
   const handleCloseModal = () => {
     setModalVisible(false);
     setTarefaEmEdicao(null);
@@ -22,9 +22,9 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={[styles.viewContainer]}>
-        <Text style={[styles.topView]}>Projeto - Teste</Text> 
+        <Text style={[styles.topView]}>Projeto - Teste</Text>
       </View>
-      <CriaTarefa 
+      <CriaTarefa
         modalVisible={modalVisible}
         onOpen={handleOpenModal}
         onClose={handleCloseModal}
